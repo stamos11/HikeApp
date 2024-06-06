17f5f599bfaf74637424799a659d8cd34f909f2a
Nicholas Stamoulis <nikolaos11@hotmail.com>
2024-06-06 20:48:24 +0300

    Initial Commit

---
+++ HikeApp/HikeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeAppApp.swift
+//  HikeApp
+//
+//  Created by stamoulis nikolaos on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
