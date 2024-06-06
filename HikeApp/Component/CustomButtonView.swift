51a992bb1037d63d8cc07bc8890184eea0a74197
Nicholas Stamoulis <nikolaos11@hotmail.com>
2024-06-06 22:12:34 +0300

    Card Header View Created

---
+++ HikeApp/Component/CustomButtonView.swift
@@ -0,0 +1,45 @@
+//
+//  CustomButtonView.swift
+//  HikeApp
+//
+//  Created by stamoulis nikolaos on 6/6/24.
+//
+
+import SwiftUI
+
+struct CustomButtonView: View {
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(LinearGradient(colors: [.white,
+                                              .customGreenLight,
+                                              .customGreenMedium],
+                                     startPoint: .top,
+                                     endPoint: .bottom)
+                )
+            Circle()
+                .stroke(LinearGradient(colors: [.customGrayLight,
+                                                .customGrayMedium],
+                                       startPoint: .top,
+                                       endPoint: .bottom),
+                                            lineWidth: 4)
+            Image(systemName: "figure.hiking")
+                .fontWeight(.black)
+                .font(.system(size: 30))
+                .foregroundStyle(
+                    LinearGradient(colors:
+                                    [ .customGrayLight,
+                                      .customGrayMedium],
+                                   startPoint: .top,
+                                   endPoint: .bottom)
+            )                        
+        }//: STACK
+        .frame(width: 58, height: 58)
+    }
+}
+
+#Preview {
+    CustomButtonView()
+        .previewLayout(.sizeThatFits)
+        .padding()
+}
