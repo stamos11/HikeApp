3bcd8dd9f19001e3001f50bf136cd5b2475831cb
Nicholas Stamoulis <nikolaos11@hotmail.com>
2024-06-06 21:28:38 +0300

    Created Card Component

---
+++ HikeApp/Component/CardView.swift
@@ -0,0 +1,41 @@
+//
+//  CardView.swift
+//  HikeApp
+//
+//  Created by stamoulis nikolaos on 6/6/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        // MARK: -CARD
+        
+        ZStack {
+            CustomBackgroundView()
+            ZStack {
+                Circle()
+                    .fill(
+                        LinearGradient(
+                            colors: [
+                                Color("ColorIndigoMedium"),
+                                Color("ColorSalmonLight")
+                            ],
+                            startPoint: .topLeading,
+                            endPoint: .bottomTrailing
+                        )
+                    )
+                    .frame(width: 256, height: 256)
+                
+                Image("image-1")
+                      .resizable()
+                  .scaledToFit()
+            }
+        } //: CARD
+        .frame(width: 320, height: 570)
+    }
+}
+
+#Preview {
+    CardView()
+}
