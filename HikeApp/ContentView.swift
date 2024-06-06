5ab99d84494602d7e485d7674ae72a309776feb2
Nicholas Stamoulis <nikolaos11@hotmail.com>
2024-06-06 20:58:48 +0300

    HikeApp setup

--- HikeApp/ContentView.swift
+++ HikeApp/ContentView.swift
@@ -9,13 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Sucess")
-        }
-        .padding()
+      Image("image-1")
+            .resizable()
+            .scaledToFit()
+      
     }
 }
 
